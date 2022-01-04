e70619c42d8c1f90bfcbc0fdb4fc71437f96e2e9
Lorenzo Milesi <lorenzo.milesi@live.fr>
2022-01-04 22:13:33 +0100

    feat(messages): ContactForm Validation and Save

--- app/Http/Livewire/Components/Site/Sections/ContactForm.php
+++ app/Http/Livewire/Components/Site/Sections/ContactForm.php
@@ -2,12 +2,60 @@
 
 namespace App\Http\Livewire\Components\Site\Sections;
 
+use App\Models\Message;
 use Livewire\Component;
 
 class ContactForm extends Component
 {
+    public Message $message;
+    public bool $isSent = false;
+
+    protected $rules = [
+        'message.first_name' => 'required|max:255',
+        'message.last_name' => 'required|max:255',
+        'message.email' => 'required|email|max:255',
+        'message.company' => 'nullable|max:255',
+        'message.phone' => 'nullable|max:20',
+        'message.content' => 'required|max:500',
+        'message.budget' => 'nullable|max:255',
+        'message.channel' => 'nullable|max:255'
+    ];
+
+    protected $messages = [
+        'message.first_name.required' => 'Le prénom est obligatoire',
+        'message.first_name.max' => 'Le prénom est limité à :max caractères',
+        'message.last_name.required' => 'Le nom est obligatoire',
+        'message.last_name.max' => 'Le nom est limité à :max caractères',
+        'message.email.required' => 'L\'email est obligatoire',
+        'message.email.email' => 'L\'email doit être valide pour vous contacter ;)',
+        'message.email.max' => 'L\'email est limité à :max caractères',
+        'message.company.max' => 'Le nom de l\'entreprise est limité à :max caractères',
+        'message.phone.max' => 'Le téléphone est limité à :max caractères',
+        'message.content.required' => 'Il me faudrait un peu de contexte pour une première approche de votre besoin',
+        'message.content.max' => 'Je suis sûr que vous pouvez me le dire en moins de :max caractères :D',
+        'message.budget.max' => 'La description du budget est limitée à :max caractères',
+        'message.channel.max' => 'Je suis sûr que vous pouvez me le dire en moins de :max caractères :D',
+    ];
+
+    public function mount()
+    {
+        $this->message = new Message();
+    }
+
+    public function updated($property)
+    {
+        $this->validateOnly($property);
+    }
+
     public function render()
     {
         return view('livewire.components.site.sections.contact-form');
+    }
+
+    public function send()
+    {
+        $this->validate();
+        $this->message->save();
+        $this->isSent = true;
     }
 }
--- resources/views/livewire/components/site/sections/contact-form.blade.php
+++ resources/views/livewire/components/site/sections/contact-form.blade.php
@@ -13,92 +13,154 @@
                 <p class="mt-4 text-lg text-gray-500 sm:mt-3">
                     Je serai ravi de vous aider. Laissez-moi un message via le formulaire ci-dessous et nous pourrons peut-être commencer quelquechose :)
                 </p>
-                <form action="#" method="POST" class="mt-9 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+                @if($isSent)
+                <p class="mt-4 text-lg text-gray-500 sm:mt-3 bg-green-200">
+                    Votre message a bien été envoyé, merci, je vous contacte dès que possible !
+                </p>
+                @else
+                <form wire:submit.prevent="send" class="mt-9 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
                     <div>
-                        <label for="first-name" class="block text-sm font-medium text-gray-700">Prénom</label>
+                        <label for="first_name" class="block text-sm font-medium text-gray-700 @error('message.first_name') text-red-500 font-bold @enderror">
+                            Prénom *
+                        </label>
                         <div class="mt-1">
-                            <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
+                            <input type="text" wire:model="message.first_name" id="first_name" autocomplete="given-name" class="block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md @error('message.first_name') border-red-300 focus:border-red-500 focus:ring-red-500 @enderror" required>
                         </div>
+                        @error('message.first_name')
+                        <div class="text-red-400 p-1">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </div>
                     <div>
-                        <label for="last-name" class="block text-sm font-medium text-gray-700">Nom</label>
+                        <label for="last_name" class="block text-sm font-medium text-gray-700 @error('message.last_name') text-red-500 font-bold @enderror">
+                            Nom *
+                        </label>
                         <div class="mt-1">
-                            <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
+                            <input type="text" wire:model="message.last_name" id="last_name" autocomplete="given-name" class="block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md @error('message.last_name') border-red-300 focus:border-red-500 focus:ring-red-500 @enderror" required>
                         </div>
+                        @error('message.last_name')
+                        <div class="text-red-400 p-1">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </div>
                     <div class="sm:col-span-2">
-                        <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                        <label for="email" class="block text-sm font-medium text-gray-700  @error('message.email') text-red-500 font-bold @enderror">
+                            Email *
+                        </label>
                         <div class="mt-1">
-                            <input id="email" name="email" type="email" autocomplete="email" class="block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
+                            <input id="email" wire:model="message.email" type="email" autocomplete="email" class="block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md @error('message.email') border-red-300 focus:border-red-500 focus:ring-red-500 @enderror">
                         </div>
+                        @error('message.email')
+                        <div class="text-red-400 p-1">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </div>
                     <div class="sm:col-span-2">
                         <div class="flex justify-between">
-                            <label for="company" class="block text-sm font-medium text-gray-700">Entreprise</label>
+                            <label for="company" class="block text-sm font-medium text-gray-700 @error('message.company') text-red-500 font-bold @enderror">Entreprise</label>
                             <span id="company-description" class="text-sm text-gray-500">Facultatif</span>
                         </div>
                         <div class="mt-1">
-                            <input type="text" name="company" id="company" autocomplete="organization" class="block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
+                            <input type="text" wire:model="message.company" id="company" autocomplete="organization" class="block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md @error('message.company') border-red-300 focus:border-red-500 focus:ring-red-500 @enderror">
                         </div>
+                        @error('message.company')
+                        <div class="text-red-400 p-1">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </div>
                     <div class="sm:col-span-2">
                         <div class="flex justify-between">
-                            <label for="phone" class="block text-sm font-medium text-gray-700">Téléphone</label>
+                            <label for="phone" class="block text-sm font-medium text-gray-700 @error('message.phone') text-red-500 font-bold @enderror">Téléphone</label>
                             <span id="phone-description" class="text-sm text-gray-500">Facultatif</span>
                         </div>
                         <div class="mt-1">
-                            <input type="text" name="phone" id="phone" autocomplete="tel" aria-describedby="phone-description" class="block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
+                            <input type="text" wire:model="message.phone" id="phone" autocomplete="tel" aria-describedby="phone-description" class="block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md @error('message.phone') border-red-300 focus:border-red-500 focus:ring-red-500 @enderror">
                         </div>
+                        @error('message.phone')
+                        <div class="text-red-400 p-1">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </div>
                     <div class="sm:col-span-2">
                         <div class="flex justify-between">
-                            <label for="how-can-we-help" class="block text-sm font-medium text-gray-700">
+                            <label for="content" class="block text-sm font-medium text-gray-700 @error('message.content') text-red-500 font-bold @enderror">
                                 Comment puis-je vous aider ?
                             </label>
-                            <span id="how-can-we-help-description" class="text-sm text-gray-500">0/500</span>
+                            <span id="content-description" class="text-sm text-gray-500 @if(strlen($message->content) > 500) text-red-500 font-bold @endif">
+                                {{ strlen($message->content) }}/500
+                            </span>
                         </div>
                         <div class="mt-1">
-                            <textarea id="how-can-we-help" name="how-can-we-help" aria-describedby="how-can-we-help-description" rows="4" class="block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border border-gray-300 rounded-md"></textarea>
+                            <textarea id="content" wire:model="message.content" aria-describedby="content-description" rows="10" class="block w-full shadow-sm sm:text-sm focus:ring-indigo-500 focus:border-indigo-500 border border-gray-300 rounded-md @error('message.content') border-red-300 focus:border-red-500 focus:ring-red-500 @enderror" required></textarea>
                         </div>
+                        @error('message.content')
+                        <div class="text-red-400 p-1">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </div>
                     <fieldset class="sm:col-span-2">
-                        <legend class="block text-sm font-medium text-gray-700">
+                        <legend class="block text-sm font-medium text-gray-700 @error('message.budget') text-red-500 font-bold @enderror">
                             Budget estimé
                         </legend>
+                        @error('message.budget')
+                        <div class="text-red-400 p-1">
+                            {{ $message }}
+                        </div>
+                        @enderror
                         <div class="mt-4 grid grid-cols-1 gap-y-4">
                             <div class="flex items-center">
-                                <input id="budget-under-2k" name="budget" value="under_2k" type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300">
+                                <input id="budget-under-2k" wire:model="message.budget" value="under_2k" type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300">
                                 <label for="budget-under-2k" class="ml-3">
                                     <span class="block text-sm text-gray-700"> - de 2K€</span>
                                 </label>
                             </div>
                             <div class="flex items-center">
-                                <input id="budget-2k-5k" name="budget" value="2k-5k" type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300">
+                                <input id="budget-2k-5k" wire:model="message.budget" value="2k-5k" type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300">
                                 <label for="budget-2k-5k" class="ml-3">
                                     <span class="block text-sm text-gray-700">entre 2K€ et 5K€</span>
                                 </label>
                             </div>
                             <div class="flex items-center">
-                                <input id="budget-5k-10k" name="budget" value="5k-10k" type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300">
+                                <input id="budget-5k-10k" wire:model="message.budget" value="5k-10k" type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300">
                                 <label for="budget-5k-10k" class="ml-3">
                                     <span class="block text-sm text-gray-700">entre 5K€ et 10K€</span>
                                 </label>
                             </div>
                             <div class="flex items-center">
-                                <input id="budget-over-10k" name="budget" value="over_10k" type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300">
+                                <input id="budget-over-10k" wire:model="message.budget" value="over_10k" type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300">
                                 <label for="budget-over-10k" class="ml-3">
                                     <span class="block text-sm text-gray-700">+ de 10K€</span>
+                                </label>
+                            </div>
+                            <div class="flex items-center">
+                                <input id="budget-null" wire:model="message.budget" value="" type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300">
+                                <label for="budget-null" class="ml-3">
+                                    <span class="block text-sm text-gray-700">Aucune idée</span>
                                 </label>
                             </div>
                         </div>
                     </fieldset>
                     <div class="sm:col-span-2">
-                        <label for="how-did-you-hear-about-us" class="block text-sm font-medium text-gray-700">
+                        <div class="flex justify-between">
+                            <label for="channel" class="block text-sm font-medium text-gray-700 @error('message.channel') text-red-500 font-bold @enderror">
                             Comment avez-vous entendu parler de moi ?
-                        </label>
-                        <div class="mt-1">
-                            <input type="text" name="how-did-you-hear-about-us" id="how-did-you-hear-about-us" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
+                            </label>
+                            <span id="company-description" class="text-sm text-gray-500">Facultatif</span>
                         </div>
+                        <div class="mt-1">
+                            <input type="text" wire:model="message.channel"  id="channel" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('message.channel') border-red-300 focus:border-red-500 focus:ring-red-500 @enderror">
+                        </div>
+                        @error('message.channel')
+                        <div class="text-red-400 p-1">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </div>
                     <div class="text-right sm:col-span-2">
                         <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
@@ -106,6 +168,7 @@
                         </button>
                     </div>
                 </form>
+                @endif
             </div>
         </div>
     </div>
