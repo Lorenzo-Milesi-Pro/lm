444b6ca221fc7d127416f9767f6055067e9b8f89
Lorenzo Milesi <lmilesi@dataneo.fr>
2022-06-21 18:17:01 +0200

    fix(posts): get only published posts for guests

--- app/Http/Livewire/Components/Site/Sections/Posts.php
+++ app/Http/Livewire/Components/Site/Sections/Posts.php
@@ -10,7 +10,7 @@ class Posts extends Component
     public function render()
     {
         return view('livewire.components.site.sections.posts', [
-            'posts' => Post::orderBy('updated_at', 'DESC')->take(3)->get()
+            'posts' => Post::whereNotNull('published_at')->orderBy('updated_at', 'DESC')->take(3)->get()
         ]);
     }
 }
