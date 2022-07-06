6ef2a6cdf4afe7e11b803677eed636ecb563d2c5
Lorenzo Milesi <lmilesi@dataneo.fr>
2022-07-06 13:30:20 +0200

    feat(post): add torchlight inside posts content
    allows users to interact with code more easily

--- resources/views/site/blog/post.blade.php
+++ resources/views/site/blog/post.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <div class="relative py-16 bg-white overflow-hidden">
         <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
-            <div class="relative h-full text-lg max-w-prose mx-auto" aria-hidden="true">
+            <div class="relative h-full text-lg max-w-screen-sm mx-auto" aria-hidden="true">
                 <svg class="absolute top-12 left-full transform translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
                     <defs>
                         <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
@@ -29,7 +29,7 @@
             </div>
         </div>
         <div class="relative px-4 sm:px-6 lg:px-8">
-            <div class="text-lg max-w-prose mx-auto">
+            <div class="text-lg max-w-screen-sm mx-auto">
                 <h1>
                     <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">{{ $post->blog_domain->name }}</span>
                     <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">{{ $post->title }}</span>
@@ -38,11 +38,17 @@
                     {{ $post->excerpt }}
                 </p>
             </div>
-            <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+            <div class="mt-6 max-w-screen-md prose prose-indigo prose-lg text-gray-500 mx-auto">
                 <img class="w-full rounded-lg" src="{{ $post->cover }}" alt="" width="1310" height="873">
-                <x-markdown>
-                    {!! $post->content !!}
-                </x-markdown>
+                @foreach($post->html_content as $k => $block)
+                    @if($k % 2)
+                    <pre><x-torchlight-code class="text-sm" language="{{ $block['language']}}">{!! $block['content'] !!}</x-torchlight-code></pre>
+                    @else
+                    <x-markdown>
+                        {!! $block !!}
+                    </x-markdown>
+                    @endif
+                @endforeach
             </div>
         </div>
     </div>
